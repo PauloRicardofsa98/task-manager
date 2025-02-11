a6ea94d2daff676b89b16e42fb3cdd2807f0c0f1
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-11 19:55:25 -0300

    feat: add toast notifications for task status updates and deletion in Tasks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import SideBar from "./components/Sidebar";
 import Tasks from "./components/Tasks";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <div className="flex">
       <SideBar />
       <Tasks />
+      <Toaster
+        toastOptions={{
+          style: {
+            color: "#35383E",
+          },
+        }}
+      />
     </div>
   );
 };
