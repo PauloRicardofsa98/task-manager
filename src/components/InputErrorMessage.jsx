07b865b5de28154602bdda18e5d2f3cd0cc3c961
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-12 15:51:39 -0300

    chore: add PropTypes validation to InputErrorMessage component for improved type safety

--- src/components/InputErrorMessage.jsx
+++ src/components/InputErrorMessage.jsx
@@ -1,5 +1,10 @@
+import PropTypes from "prop-types";
+
 const InputErrorMessage = ({ message }) => {
   return <span className="text-left text-xs text-red-500">{message}</span>;
+};
+InputErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
 };
 
 export default InputErrorMessage;
