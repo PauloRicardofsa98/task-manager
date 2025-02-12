cb5ef90bf95bf404ce1f2a1ccab737641f2c2ca6
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-12 15:45:07 -0300

    feat: add PropTypes validation to TasksSeparator component for improved type safety

--- src/components/TasksSeparator.jsx
+++ src/components/TasksSeparator.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const TasksSeparator = ({ icon, text }) => {
   return (
     <div className="flex gap-2 border-b border-solid border-brand-border pb-1">
@@ -5,6 +7,11 @@ const TasksSeparator = ({ icon, text }) => {
       <p className="text-sm text-brand-text-gray">{text}</p>
     </div>
   );
+};
+
+TasksSeparator.propTypes = {
+  icon: PropTypes.element.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default TasksSeparator;
