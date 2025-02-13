06b9685844ccf1f36b828805faaa3e24494957f5
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-13 12:35:31 -0300

    feat: update task statuses to 'in_progress' and add DashboardCards component for task overview

---
+++ src/components/DashboardCards.jsx
@@ -0,0 +1,41 @@
+import { LoaderIcon, Tasks2Icon, TasksIcon } from "../assets/icons";
+import { useGetTasks } from "../hooks/data/use-get-tasks";
+import DashboardCard from "./DashboardCard";
+
+const DashboardCards = () => {
+  const { data: tasks } = useGetTasks();
+
+  const notStartedTasks = tasks?.filter(
+    (task) => task.status === "not_started",
+  ).length;
+  const doneTasks = tasks?.filter((task) => task.status === "done").length;
+  const inProgressTasks = tasks?.filter(
+    (task) => task.status === "in_progress",
+  ).length;
+  return (
+    <div className="grid grid-cols-4 gap-9">
+      <DashboardCard
+        icon={<Tasks2Icon />}
+        mainText={tasks?.length}
+        secondaryText="Tarefas totais"
+      />
+      <DashboardCard
+        icon={<Tasks2Icon />}
+        mainText={notStartedTasks}
+        secondaryText="Tarefas não iniciadas"
+      />
+      <DashboardCard
+        icon={<LoaderIcon />}
+        mainText={inProgressTasks}
+        secondaryText="Tarefas em andamento"
+      />
+      <DashboardCard
+        icon={<TasksIcon />}
+        mainText={doneTasks}
+        secondaryText="Tarefas concluídas"
+      />
+    </div>
+  );
+};
+
+export default DashboardCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,41 @@
-import {
-  GlassWaterIcon,
-  LoaderIcon,
-  Tasks2Icon,
-  TasksIcon,
-} from "../assets/icons";
-import DashboardCard from "../components/DashboardCard";
+import DashboardCards from "../components/DashboardCards";
 import Header from "../components/Header";
 import SideBar from "../components/Sidebar";
+import TaskItem from "../components/TaskItem";
 import { useGetTasks } from "../hooks/data/use-get-tasks";
 
 const HomePage = () => {
   const { data: tasks } = useGetTasks();
-
-  const doneTasks = tasks?.filter((task) => task.status === "done").length;
-  const inProgressTasks = tasks?.filter(
-    (task) => task.status === "in_progress",
-  ).length;
-
   return (
     <div className="flex">
       <SideBar />
       <div className="w-full space-y-6 px-8 py-16">
         <Header subtitle="Dashboard" title="Dashboard" />
-        <div className="grid grid-cols-4 gap-9">
-          <DashboardCard
-            icon={<Tasks2Icon />}
-            mainText={tasks?.length}
-            secondaryText="Tarefas disponíveis"
-          />
-          <DashboardCard
-            icon={<TasksIcon />}
-            mainText={doneTasks}
-            secondaryText="Tarefas concluídas"
-          />
-          <DashboardCard
-            icon={<LoaderIcon />}
-            mainText={inProgressTasks}
-            secondaryText="Tarefas em andamento"
-          />
-          <DashboardCard
-            icon={<GlassWaterIcon />}
-            mainText="5"
-            secondaryText="Água"
-          />
+        <DashboardCards />
+        <div className="grid grid-cols-[1.5fr,1fr] gap-6">
+          <div className="space-y-6 rounded-[10px] bg-white p-6">
+            <div>
+              <h3 className="text-xl font-semibold">Tarefas</h3>
+              <span className="text-sm text-brand-dark-gray">
+                Resumo das tarefas disponíveis
+              </span>
+            </div>
+
+            <div className="space-y-3">
+              {tasks?.map((task) => (
+                <TaskItem key={task.id} task={task} />
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-center rounded-[10px] bg-white p-6">
+            <p className="text-brand-dark-gray">
+              Cada pequena ação de hoje te aproxima das grandes conquistas de
+              amanhã. Faça o que precisa ser feito!
+            </p>
+          </div>
         </div>
+
+        <div className="bg-white"></div>
       </div>
     </div>
   );
