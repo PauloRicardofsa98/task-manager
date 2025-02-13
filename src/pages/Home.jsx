9c2a4ea9d9ecdff682922a65cf73f8ce785f2199
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-13 12:02:42 -0300

    feat: update task data structure and add DashboardCard and Header components for improved UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,50 @@
+import {
+  GlassWaterIcon,
+  LoaderIcon,
+  Tasks2Icon,
+  TasksIcon,
+} from "../assets/icons";
+import DashboardCard from "../components/DashboardCard";
+import Header from "../components/Header";
 import SideBar from "../components/Sidebar";
+import { useGetTasks } from "../hooks/data/use-get-tasks";
 
 const HomePage = () => {
+  const { data: tasks } = useGetTasks();
+
+  const doneTasks = tasks?.filter((task) => task.status === "done").length;
+  const inProgressTasks = tasks?.filter(
+    (task) => task.status === "in_progress",
+  ).length;
+
   return (
     <div className="flex">
       <SideBar />
-      <h1>home</h1>;
+      <div className="w-full space-y-6 px-8 py-16">
+        <Header subtitle="Dashboard" title="Dashboard" />
+        <div className="grid grid-cols-4 gap-9">
+          <DashboardCard
+            icon={<Tasks2Icon />}
+            mainText={tasks?.length}
+            secondaryText="Tarefas disponíveis"
+          />
+          <DashboardCard
+            icon={<TasksIcon />}
+            mainText={doneTasks}
+            secondaryText="Tarefas concluídas"
+          />
+          <DashboardCard
+            icon={<LoaderIcon />}
+            mainText={inProgressTasks}
+            secondaryText="Tarefas em andamento"
+          />
+          <DashboardCard
+            icon={<GlassWaterIcon />}
+            mainText="5"
+            secondaryText="Água"
+          />
+        </div>
+      </div>
     </div>
   );
 };
